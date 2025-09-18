188bc2e288cf93aeec18d9e2be438aa6920f9682
Alepm22 <alejandra.palza@ucb.edu.bo>
2025-09-18 00:31:50 -0400

    Visualizacion pagina principal

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
+import { Principal } from './pages/principal/principal';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' }, 
+  /*{ path: '', redirectTo: '/inicio', pathMatch: 'full' }, 
   { path: 'inicio', component: Inicio }, 
-  { path: '**', redirectTo: '/inicio' } 
+  { path: '**', redirectTo: '/inicio' } */
+  { path: 'principal', component: Principal },
 ];
+
+
+
+
+
+
