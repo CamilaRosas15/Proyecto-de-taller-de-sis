30098f715fea4d74d047eaa56c4df68588a82b65
NickyGS2WK <nicole.gomez@wiserkronox.com>
2025-10-04 19:25:28 -0400

    Contatanos mejorado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Principal } from './pages/principal/principal';
 import { AnalisisImagenComponent } from './pages/analisis-imagen/analisis-imagen';
 import { Registro } from './pages/registro/registro';
 import { Login } from './pages/login/login';
+import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/principal', pathMatch: 'full' }, 
@@ -14,5 +15,6 @@ export const routes: Routes = [
   { path: 'analisis-imagen', component: AnalisisImagenComponent },
   { path: 'registro', component: Registro },
   { path: 'login', component: Login },
+  {path: 'sobre-nosotros', component: SobreNosotros}, 
   { path: '**', redirectTo: '/principal' } 
 ];
