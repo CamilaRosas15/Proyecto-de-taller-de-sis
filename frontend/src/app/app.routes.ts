158ab370ee752801283e84d9f6ecf94a320a0463
NickyGS2WK <nicole.gomez@wiserkronox.com>
2025-10-04 19:44:05 -0400

    Contatanos mejorado y creacion de carpeta de contactanos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Inicio } from './pages/inicio/inicio';
+//import { Inicio } from './pages/inicio/inicio';
 import { Recetas } from './pages/recetas/recetas';
 import { Principal } from './pages/principal/principal';
 import { AnalisisImagenComponent } from './pages/analisis-imagen/analisis-imagen';
@@ -9,7 +9,7 @@ import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/principal', pathMatch: 'full' }, 
-  { path: 'inicio', component: Inicio }, 
+  //{ path: 'inicio', component: Inicio }, 
   { path: 'recetas', component: Recetas }, 
   { path: 'principal', component: Principal },
   { path: 'analisis-imagen', component: AnalisisImagenComponent },
