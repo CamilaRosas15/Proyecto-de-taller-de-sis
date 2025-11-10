6bfaec40fe0807819324d8ac67b5480fc0486a9d
Laura Camacho <laura.camacho@ucb.edu.bo>
2025-11-10 14:54:08 -0400

    pagina perfil funcionando

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,7 +6,8 @@ import { AnalisisImagenComponent } from './pages/analisis-imagen/analisis-imagen
 import { Registro } from './pages/registro/registro';
 import { Login } from './pages/login/login';
 import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
-import { ChatIAComponent } from './pages/chat-ia/chat-ia'; // ← Agregar esta importación
+import { ChatIAComponent } from './pages/chat-ia/chat-ia';
+import { PerfilUsuarioComponent } from './pages/perfil-usuario/perfil-usuario'; // ← Agregar esta importación
 
 export const routes: Routes = [
   { path: '', redirectTo: '/principal', pathMatch: 'full' }, 
@@ -17,6 +18,7 @@ export const routes: Routes = [
   { path: 'registro', component: Registro },
   { path: 'login', component: Login },
   { path: 'sobre-nosotros', component: SobreNosotros }, 
-  { path: 'chat-ia', component: ChatIAComponent }, // ← Agregar esta nueva ruta
+  { path: 'chat-ia', component: ChatIAComponent },
+  { path: 'perfil', component: PerfilUsuarioComponent }, // ← Agregar esta nueva ruta
   { path: '**', redirectTo: '/principal' } 
 ];
